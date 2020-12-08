7d70a972792064a3d56a6397e87463f856c51183
Jay Peek <73311799+ChefJayPeek@users.noreply.github.com>
2020-12-07 19:50:25 -0600

    Issues commiting to heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require ("path");
-const fs = require ("path");
+const fs = require ("fs");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -19,7 +19,7 @@ app.get("/api/notes", function (req, res) {
 });
 
 app.get("/api/notes/:id", function(req, res) {
-    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let savedNotes = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
     res.jason(savedNotes[Number(req.params.id)]);
 });
 
@@ -29,6 +29,35 @@ app.get("*", function(req, res) {
 
 
 
+app.post("/api/notes", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+    let newNote = req.body;
+    let uniqueID = (savedNotes.length).toString();
+    newNote.id = uniqueID;
+    savedNotes.push(newNote);
+
+    fs.writeFileSync("db/db.json", JSON.stringify(savedNotes));
+    console.log("Note saved to db.json. Content: ", newNote);
+    res.json(savedNotes);
+})
+
+app.delete("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+    let noteID = req.params.id;
+    let newID = 0;
+    console.log(`Deleting note with ID ${noteID}`);
+    savedNotes = savedNotes.filter(currNote => {
+        return currNote.id != noteID;
+    })
+    
+    for (currNote of savedNotes) {
+        currNote.id = newID.toString();
+        newID++;
+    }
+
+    fs.writeFileSync("db/db.json", JSON.stringify(savedNotes));
+    res.json(savedNotes);
+})
 
 
 app.listen(PORT, function() {
